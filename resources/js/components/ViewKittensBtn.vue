30f3a27bb925c7529e2c229de18f359f1c0495ba
daverydan <danny.w.avery@gmail.com>
2022-06-23 10:19:21 -0400

    Update: ViewKittensBtn responsive images

--- resources/js/components/ViewKittensBtn.vue
+++ resources/js/components/ViewKittensBtn.vue
@@ -15,24 +15,28 @@
             v-if="pics.length"
             :id="`kitten-pics-${id}`"
             style="background: rgba(149, 108, 52, 0.2)"
-            class="mt-3 flex flex-wrap flex-col sm:flex-row justify-center rounded-md p-2"
+            class="cat-pic-wrapper mt-3 rounded-md p-2"
         >
+            <!-- flex flex-wrap flex-col sm:flex-row -->
+            <!-- flex-col sm:flex-row justify-center -->
             <div
                 v-for="pic in pics"
                 :key="pic.id"
                 :id="`pic-${pic.id}`"
-                class="w-full sm:w-1/2 md:w-1/4 overflow-hidden p-2 relative"
+                class="cat-pic overflow-hidden relative"
             >
+                <!-- class="w-full sm:w-1/2 md:w-1/4 overflow-hidden p-2 relative" -->
                 <img
                     :src="`/storage/litters/${pic.imageable_id}/${pic.image}`"
-                    class="w-full h-full object-center object-cover group-hover:opacity-75 rounded-md"
+                    class="w-full h-full object-center object-contain hover:opacity-75 rounded-md"
                 />
+                <!-- w-full h-full  -->
                 <a
                     v-if="loggedin"
                     @click.prevent="deletePic(pic.id)"
                     href="#"
                     class="bg-red-600 hover:bg-red-700 z-50 text-white absolute w-8 h-8 rounded-full flex justify-center items-center"
-                    style="border: 3px solid #eae1d6; right: -2px; top: -2px"
+                    style="border: 3px solid #eae1d6; right: 0; top: -2px"
                 >
                     <i class="fa fa-times"></i>
                 </a>
@@ -89,3 +93,29 @@ export default {
     },
 };
 </script>
+
+<style>
+.cat-pic-wrapper {
+    overflow: auto;
+}
+.cat-pic {
+    margin-bottom: 20px;
+    padding: 0 1%;
+    width: 100%;
+}
+@media (min-width: 1024px) {
+    .cat-pic {
+        float: left;
+        margin: 12px 0;
+        width: 25%;
+    }
+}
+@media (min-width: 640px) and (max-width: 1023px) {
+    .cat-pic {
+        float: left;
+        margin: 12px 0;
+        padding: 0 1.5%;
+        width: 50%;
+    }
+}
+</style>
